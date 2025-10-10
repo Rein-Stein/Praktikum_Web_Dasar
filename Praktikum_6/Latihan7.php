be011e17f9bde32a14af9fac78f9b8580ae856b5
Muhammad F M <2411102441153@umkt.ac.id>
2025-10-10 22:16:25 +0800

    Add files via upload

---
+++ Praktikum_6/Latihan7.php
@@ -0,0 +1,17 @@
+<?php
+echo"Hello World 1! </br>"; // perintah ini akan mencetak hello world
+?>
+
+<?php
+/*
+berikut ini adalah perintah untukmenampilkan teks hello world pada browser
+*/
+echo "Hello World 2! </br>";
+?>
+
+<?php
+// Berikut ini adalah perintah
+// untuk menampilkan teks hello world
+// pada browser
+echo"Hello World 3!";
+?>
